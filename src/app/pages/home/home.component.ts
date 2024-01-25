e6b89645f08e1d972b575a9e965523a008dba884
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-01-25 14:57:17 -0400

    feat: Add Condição IF

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormComponent } from '../../components/form/form.component';
 import { CardComponent } from '../../components/card/card.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ComponenteService } from '../../services/componente.service';
 import { ComponenteInterface } from '../../types/componente';
 import { HttpClientModule } from '@angular/common/http';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent, CardComponent, NgFor, HttpClientModule, MatGridListModule],
+  imports: [FormComponent, CardComponent, NgFor, HttpClientModule, MatGridListModule, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor(private componenteService: ComponenteService) {}
+  destroyed = new Subject<void>();
+  currentScreenSize: string = '';
+  loading: boolean = true;
+
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'XSmall'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
+
+  constructor(private componenteService: ComponenteService, private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+        .observe([
+          Breakpoints.XSmall,
+          Breakpoints.Small,
+          Breakpoints.Medium,
+          Breakpoints.Large,
+          Breakpoints.XLarge,
+        ])
+        .pipe(takeUntil(this.destroyed))
+        .subscribe((result) => {
+          for (const query of Object.keys(result.breakpoints)) {
+            if (result.breakpoints[query]) {
+              this.displayNameMap.get(query) ?? 'Unknown';
+            }
+          }
+        })
+  }
 
   listaComponente: ComponenteInterface[] = [];
 
   ngOnInit(): void {
     this.componenteService.listar().subscribe((res) => {
+      this.loading = true;
+      console.log('LOADING ', this.loading);
       this.listaComponente = res;
+      this.loading = false;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
   }
 }
